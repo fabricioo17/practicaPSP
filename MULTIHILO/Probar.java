67980ab61bb9aa7bcc2eaf8b5bddf9a309447d95
fabricioo17 <168642413+fabricioo17@users.noreply.github.com>
2025-01-13 14:06:50 +0100

    Create Probar.java

---
+++ MULTIHILO/Probar.java
@@ -0,0 +1,20 @@
+
+public class Probar {
+
+
+    public static void main(String[] args) {
+        Deposito deposito = new Deposito();
+        Camion camion1=new Camion(deposito,1);
+
+        Camion camion2=new Camion(deposito,2);
+
+        Camion camion3=new Camion(deposito,3);
+
+        ProductorG productorG=new ProductorG(deposito);
+
+        productorG.start();
+        camion1.start();
+        camion2.start();
+        camion3.start();
+    }
+}
