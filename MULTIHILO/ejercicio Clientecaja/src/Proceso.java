1b4f0fac23bf168572de8556a67093c4733a7b68
fabricioo17 <168642413+fabricioo17@users.noreply.github.com>
2025-01-20 14:06:06 +0100

    ejercicio caja/cliente

--- MULTIHILO/ejercicio Clientecaja/src/Proceso.java
+++ MULTIHILO/ejercicio Clientecaja/src/Proceso.java
@@ -12,20 +12,19 @@ public class Proceso {
             wait();
         }
         System.out.println("el cliente "  + identificador + " esta pagando");
-        Thread.sleep(5000);
-
-        System.out.println("cliente  " + identificador + " termino");
-        cajaDisponible =true;
+        cajaDisponible =false;
         notifyAll();
     }
 
-    public synchronized boolean cambiarLibre(Boolean libre, int identificador) throws InterruptedException {
+    public synchronized boolean cambiarLibre( int identificador) throws InterruptedException {
 
-        while (cajaDisponible ==true){
-            System.out.println("la caja " + identificador + " esta libre");
+        while (cajaDisponible ==false){
+            System.out.println("la caja " + identificador + " esta ocupado");
             wait();
         }
-        cajaDisponible=false;
+        System.out.println("la caja " + identificador + " libre");
+        Thread.sleep(5000);
+        cajaDisponible=true;
         notifyAll();
        return cajaDisponible;
     }
