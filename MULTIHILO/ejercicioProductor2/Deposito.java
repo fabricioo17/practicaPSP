cd9ea7b6d53ccadc1c3db7ba197bf91ea48e513a
fabricioo17 <168642413+fabricioo17@users.noreply.github.com>
2025-01-20 12:22:59 +0100

    Update Deposito.java

--- MULTIHILO/ejercicioProductor2/Deposito.java
+++ MULTIHILO/ejercicioProductor2/Deposito.java
@@ -12,53 +12,36 @@ public class Deposito extends Thread {
 
     
     public synchronized double get(int identificador) throws InterruptedException {
-        while ((litrosdeposito == 0) ||
-                (identificador == 1 && !consumir1) ||
-                (identificador == 2 && !consumir2) ||
-                (identificador == 3 && !consumir3)) {
+        while ((litrosdeposito == 0)                ) {
             System.out.println("El camión " + identificador + " está esperando");
             wait();
         }
 
        
         double litrosConsumidos = litrosdeposito;
-        litrosdeposito = 0; // Vaciar el depósito 
-
-        // Actualiza el estado del camión que consumió
-        if (identificador == 1) {
-            consumir1 = false; // Este camión ya consumió
-            consumir2 = true;  // El siguiente listo para consumir
-            consumir3 = true;
-        } else if (identificador == 2) {
-            consumir2 = false;
-            consumir1 = true;
-            consumir3 = true;
-        } else if (identificador == 3) {
-            consumir3 = false;
-            consumir1 = true;
-            consumir2 = true;
-        }
-
         System.out.println("El camión " + identificador + " consumió " + litrosConsumidos + " litros");
+        litrosdeposito = 0; // Vaciar el depósito       
         notifyAll(); 
         return litrosConsumidos;
     }
 
    
     public synchronized void put() throws InterruptedException {
-        Random random=  new Random();
-        int litros = random.nextInt(10) + 1;
-      
-        while (litrosdeposito > 0) {
+    
+    
+     	while (litrosdeposito > 0) {
             System.out.println("Depósito lleno, esperando a que un camión consuma");
             wait();
         }
-
-      
+        
+        Random random=  new Random();
+        int litros = random.nextInt(10) + 1;
+       
+     
         litrosdeposito = litros;
-
         System.out.println("El productor produjo " + litros + " litros");
         notifyAll(); // Notifica a los camiones que ya pueden consumir
     }
 }
+
 
