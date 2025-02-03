ea41b8344d8036039d516f316374f4306122a6e6
fabricioo17 <168642413+fabricioo17@users.noreply.github.com>
2025-02-03 13:43:04 +0100

    Update Cliente2.java

---
+++ SOCKETS/EJERCICIOS_SOCKETS/src/ejercicio1/Cliente2.java
@@ -0,0 +1,25 @@
+package ejercicio1;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Cliente2 {
+    public static void main(String[] args) throws IOException {
+        Socket socket=new Socket("localhost",5000);
+
+        DataInputStream entrada = new DataInputStream(socket.getInputStream());
+        int valor=entrada.readInt();
+        System.out.println(factorial(valor));
+    }
+
+
+    public static int factorial(int valor){
+        int num=1;
+        for (int i=1;i<=valor;i++ ){
+            num=num*i;
+        }
+
+        return num;
+    }
+}
