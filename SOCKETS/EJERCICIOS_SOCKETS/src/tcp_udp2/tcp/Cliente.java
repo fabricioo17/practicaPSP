85c708bb6633fefe3e81e5e02a5a759c06ac6818
fabricioo17 <168642413+fabricioo17@users.noreply.github.com>
2025-02-04 13:53:03 +0100

    ejercicio 3 tcp

--- SOCKETS/EJERCICIOS_SOCKETS/src/tcp_udp2/tcp/Cliente.java
+++ SOCKETS/EJERCICIOS_SOCKETS/src/tcp_udp2/tcp/Cliente.java
@@ -1,4 +1,30 @@
 package tcp_udp2.tcp;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import java.net.Socket;
+
 public class Cliente {
+    public static void main(String[] args) throws IOException {
+
+        Socket socket = new Socket("localhost",5555);
+        DataOutputStream salida = new DataOutputStream(socket.getOutputStream());
+        int random= (int) ((Math.random()*10)+1);
+        byte[] numeros = new  byte[5];
+
+        for (int i =0;i<5;i++){
+            System.out.println("numero añadido " + random );
+            numeros[i]= (byte) random;
+            random= (int) ((Math.random()*10)+1);
+        }
+        salida.write(numeros);
+
+        salida.flush(); // Asegurar que se envíen los datos antes de cerrar
+
+        socket.close();
+        salida.close();
+
+
+    }
 }
--- SOCKETS/EJERCICIOS_SOCKETS/src/tcp_udp2/tcp/Servidor.java
+++ SOCKETS/EJERCICIOS_SOCKETS/src/tcp_udp2/tcp/Servidor.java
@@ -4,21 +4,56 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+
 public class Servidor {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket= new ServerSocket(5555);
-        Socket socket=serverSocket.accept();
-        DataInputStream entrada = new DataInputStream(socket.getInputStream());
-        int mensaje = entrada.readInt();
-        System.out.println("el mensaje es " + mensaje);
-        entrada.close();
-        while (true){
-            socket=serverSocket.accept();
-            entrada=new DataInputStream(socket.getInputStream());
-            mensaje=entrada.readInt();
-            System.out.println("el mensaje es " + mensaje);
+        ServerSocket serverSocket = new ServerSocket(5555);
+        byte[] recibido = new byte[1024];
+        int mensaje;
+        while (true) {
+            Socket socket = serverSocket.accept();
+            DataInputStream entrada = new DataInputStream(socket.getInputStream());
+            recibido = entrada.readAllBytes(); // ← Aquí está la corrección
+            System.out.println("la suma es " + Suma(recibido));
+            System.out.println("el mayor es " + Mayor(recibido));
+            System.out.println("el menor es " + Menor(recibido));
             entrada.close();
         }
 
+
+
+
     }
+
+
+    public static int Suma (byte[]numeros){
+        int suma=0;
+        for (int i=0 ; i <numeros.length;i++){
+            System.out.println("numero recibido " + numeros[i]);
+            suma=suma   + numeros[i];
+        }
+        return suma;
+    }
+
+
+
+    public  static int Mayor(byte [ ] numeros){
+        int mayor=numeros[0],menor=numeros[0];
+        for (int i=1 ; i <numeros.length;i++){
+            if (numeros[i]>mayor){
+                mayor=numeros[i];
+            }
+        }
+        return  mayor;
+    }
+    public  static int Menor(byte [ ] numeros){
+        int menor=numeros[0];
+        for (int i=1 ; i <numeros.length;i++){
+            if (menor>numeros[i]){
+                menor=numeros[i];
+            }
+        }
+        return  menor;
+    }
+
 }
