54deaab09a846eee7dea4b1a6b3f84863834a6b6
fabricioo17 <168642413+fabricioo17@users.noreply.github.com>
2025-02-10 12:08:52 +0100

    actualizar ejercicio 3 udp

--- SOCKETS/EJERCICIOS_SOCKETS/src/tcp_udp3/udp/Servidor.java
+++ SOCKETS/EJERCICIOS_SOCKETS/src/tcp_udp3/udp/Servidor.java
@@ -10,33 +10,33 @@ import java.net.SocketException;
 
 public class Servidor {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        // Crear el socket para recibir datos en el puerto 5555
-        DatagramSocket socket = new DatagramSocket(5555);
-        byte[] data = new byte[1024];  // Buffer para recibir datos
 
-        // Crear el DatagramPacket para almacenar el paquete recibido
+        DatagramSocket socket = new DatagramSocket(5555);
+        byte[] data = new byte[1024];  // recibir datos
+
+
         DatagramPacket packet = new DatagramPacket(data, data.length);
 
-        // Esperar a recibir el paquete
+        // recibir el paquete
         socket.receive(packet);
 
-        // Crear un ByteArrayInputStream para leer los datos recibidos
+        //  leer los datos recibidos
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data, 0, data.length);
 
 
-        // Crear un ObjectInputStream para deserializar el objeto
+        //  deserializar el objeto
         ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
 
-        // Leer el objeto de tipo Factura
+
         Factura recibido = (Factura) objectInputStream.readObject();
 
-        // Mostrar el objeto recibido (si es necesario)
+
         System.out.println("Factura recibida: " + recibido.getNombre());
 
-        // Cerrar el ObjectInputStream
+
         objectInputStream.close();
 
-        // Cerrar el socket
+        
         socket.close();
     }
 }
