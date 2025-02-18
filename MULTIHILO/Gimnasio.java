138f453e011f6854cd194d460908692b21b9b9ce
fabricioo17 <168642413+fabricioo17@users.noreply.github.com>
2025-02-18 14:02:53 +0100

    Create Gimnasio.java

---
+++ MULTIHILO/Gimnasio.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Gimnasio {
+    private final ArrayBlockingQueue<Integer> cola = new ArrayBlockingQueue<>(5);
+    private boolean lleno = false; // Controla si la cola está llena
+
+    public Gimnasio() {
+
+    }
+
+    public synchronized void entrar(int id) throws InterruptedException {
+        while (lleno==true){
+            System.out.println("cliente " + id + " esta esperando");
+            wait();
+        }
+        cola.put(id);
+        System.out.println("el cliente " + id + " esta dentro ");
+        notifyAll();
+    }
+
+    public synchronized void salir(int id) throws InterruptedException {
+        cola.take();
+        notifyAll();
+        System.out.println("cliente " + id + "ha salido");
+    }
+}
